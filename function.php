182dc8fd441069be820fb1e1f1b0d7640c94907f
e2295531 <e2295531@cmaisonneuve.qc.ca>
2022-05-18 13:54:59 -0400

    ajout de commentaire

--- function.php
+++ function.php
@@ -1,5 +1,6 @@
 <?php
 
+/*fontion pour valider le mots de passe*/
 function checkMDP($mdp)
 {
     $symboles = ["a", "b", "c", "d", "e", "f", "g", "i", "j", "k", "h", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
